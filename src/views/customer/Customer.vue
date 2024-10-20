1eb924f49e357bab5c491c796b28b37cfb8e791b
MohammadKhattab <110059388+MohammadKhattabK@users.noreply.github.com>
2024-10-20 21:10:30 +0400

    add component profile and responsive table and add search to table

--- src/views/customer/Customer.vue
+++ src/views/customer/Customer.vue
@@ -9,7 +9,7 @@ import ChatIcon from '@/assets/icons/chat.svg'
 import GroupChatIcon from '@/assets/icons/group-chat.svg'
 import SettingsIcon from '@/assets/icons/settings.svg'
 import LogoutIcon from '@/assets/icons/logout.svg'
-import PlusIcon from '@/assets/icons/plus.svg' 
+import PlusIcon from '@/assets/icons/plus.svg'
 import FilterIcon from '@/assets/icons/filter.svg'
 import FigmaIcon from '@/assets/icons/figma.svg'
 import SketchIcon from '@/assets/icons/sketch.svg'
@@ -19,9 +19,13 @@ import TrashIcon from '@/assets/icons/trash.svg'
 import ChevronLeftIcon from '@/assets/icons/chevron-left.svg'
 import ChevronRightIcon from '@/assets/icons/chevron-right.svg'
 import imageItem from '@/assets/img/background-login.jpg'
+import {ChevronDownIcon, SearchIcon } from 'lucide-vue-next'
 
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import DialogDelete from '@/components/dialogDelete/DialogDelete.vue'
+
+const router = useRouter();
 
 const showModal = ref(false)
 
@@ -33,10 +37,13 @@ const closeDeleteDialog = () => {
   showModal.value = false
 }
 
+const goToProfile = () => {
+  router.push({ name: 'Profile' })
+}
+
 const tabLabels = ['قائمة الزبائن', 'المدفوعات', 'المستحقات']
 
 const activeTab = ref(0)
- 
 
 const products = [
   {
@@ -86,7 +93,35 @@ const products = [
   },
 ]
 
+const searchQuery = ref('')
+const selectedCategory = ref('')
+const showCategoryDropdown = ref(false)
 
+const categories = [
+  'جميع الأصناف',
+  'إلكترونيات',
+  'ملابس',
+  'أثاث',
+  'كتب',
+  'أدوات منزلية'
+]
+
+const toggleCategoryDropdown = () => {
+  showCategoryDropdown.value = !showCategoryDropdown.value
+}
+
+const selectCategory = (category: string) => {
+  selectedCategory.value = category
+  showCategoryDropdown.value = false
+}
+
+const handleSearch = () => {
+  console.log('Searching for:', searchQuery.value, 'in category:', selectedCategory.value) 
+}
+
+const createNewProduct = () => {
+  console.log('Creating new product') 
+}
 </script>
 
 <template src="./Customer.html"></template>
